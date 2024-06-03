8a7d351601544ed71045cc848da58ac1c4d6e275
rytkmt <r12tkmt@gmail.com>
2024-06-03 14:18:34 +0900

    Change ruby color

--- lua/lighthouse/hlgroups/treesitter/ruby.lua
+++ lua/lighthouse/hlgroups/treesitter/ruby.lua
@@ -7,7 +7,7 @@ function M.mapping()
   local mapping = {}
   mapping['@variable.member.ruby'] = { fg = colors.yellow } -- インスタンス変数
   mapping['@label'] = { fg = colors.green } -- here doc
-
+  mapping['@keyword.import'] = { fg = colors.green } -- require
   return mapping
 end
 
